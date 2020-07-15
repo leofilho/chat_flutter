40f45002f4590c01ac1cc072e7700f62ac9fa8f1
Leonardo Vieira <60668645+leofilho@users.noreply.github.com>
2020-07-14 22:42:04 -0300

    Commit_chat

--- lib/main.dart
+++ lib/main.dart
@@ -1,17 +1,119 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
-void main() {
-  WidgetsFlutterBinding.ensureInitialized();
-  Firestore.instance.collection("mesagem").document("ddd").setData({"teste":"teste", "texto":"leo"});
 
+void main() {
   runApp(MyApp());
 }
+
+final ThemeData kIOSThema = ThemeData(
+  primarySwatch: Colors.orange,
+  primaryColor: Colors.grey[100],
+  primaryColorBrightness: Brightness.light,
+);
+
+final ThemeData kDefaultTheme = ThemeData(
+  primarySwatch: Colors.purple,
+  accentColor: Colors.orangeAccent[400],
+);
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container(
-      
+    return MaterialApp(
+      title: "Chat App",
+      debugShowCheckedModeBanner: false,
+      theme: Theme.of(context).platform == TargetPlatform.iOS
+          ? kIOSThema
+          : kDefaultTheme,
+      home: ChatScreen(),
+    );
+  }
+}
+
+class ChatScreen extends StatefulWidget {
+  @override
+  _ChatScreenState createState() => _ChatScreenState();
+}
+
+class _ChatScreenState extends State<ChatScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      bottom: false,
+      top: false,
+      child: Scaffold(
+        appBar: AppBar(
+          title: Text("Chat App"),
+          centerTitle: true,
+          elevation:
+              Theme.of(context).platform == TargetPlatform.iOS ? 0.0 : 4.0,
+        ),
+        body: Column(
+          children: <Widget>[
+            Container(
+              decoration: BoxDecoration(
+                color: Theme.of(context).cardColor,
+              ),
+              child: TextComposer(),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class TextComposer extends StatefulWidget {
+  @override
+  _TextComposerState createState() => _TextComposerState();
+}
+
+class _TextComposerState extends State<TextComposer> {
+  bool _isCompoosing = false;
+
+  @override
+  Widget build(BuildContext context) {
+    return IconTheme(
+      data: IconThemeData(color: Theme.of(context).accentColor),
+      child: Container(
+        margin: const EdgeInsets.symmetric(horizontal: 8.0),
+        decoration: Theme.of(context).platform == TargetPlatform.iOS
+            ? BoxDecoration(
+                border: Border(top: BorderSide(color: Colors.grey[200])))
+            : null,
+        child: Row(
+          children: <Widget>[
+            Container(
+              child: IconButton(
+                icon: Icon(Icons.photo_camera),
+                onPressed: () {},
+              ),
+            ),
+            Expanded(
+              child: TextField(
+                decoration:
+                    InputDecoration.collapsed(hintText: "Enviar uma mensagem"),
+                onChanged: (text) {
+                  setState(() {
+                    _isCompoosing = text.length > 0;
+                  });
+                },
+              ),
+            ),
+            Container(
+              margin: const EdgeInsets.symmetric(horizontal: 4.0),
+              child: Theme.of(context).platform == TargetPlatform.iOS
+                  ? CupertinoButton(
+                      child: Text("Enviar"),
+                      onPressed: _isCompoosing ? () {} : null)
+                  : IconButton(
+                      icon: Icon(Icons.send),
+                      onPressed: _isCompoosing ? () {} : null),
+            )
+          ],
+        ),
+      ),
     );
   }
 }
